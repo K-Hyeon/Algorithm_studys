101c72f28cd6c3b8f55358977a239d021cb61970
Seongyun <120894109+Timeisfast@users.noreply.github.com>
2026-08-12 15:11:53 +0900

    [D3] Title: N-Queen, Time: 96 ms, Memory: 60,928 KB -BaekjoonHub

--- seongyun/SWEA/D3/2806. N－Queen/N－Queen.py
+++ seongyun/SWEA/D3/2806. N－Queen/N－Queen.py
@@ -1,29 +1,33 @@
-def recursive(location, N, n):
-    global ans
-    if n == N:
-        ans += 1
-        return
+def recursive(row, N, visited_col, visited_diag1, visited_diag2):
+    if row == N:
+        return 1
 
-    for i in range(N):
-        for loc in location:
-            x_diff = loc[0] - n
-            y_diff = loc[1] - i
-            if y_diff == 0 or x_diff + y_diff == 0 or x_diff - y_diff == 0:
-                break
-        else:
-            location.add((n, i))
-            recursive(location, N, n + 1)
-            location.remove((n, i))
+    cnt = 0
+    for col in range(N):
+        if not visited_col[col] and not visited_diag1[row + col] and not visited_diag2[row - col + N]:
+            visited_col[col] = True
+            visited_diag1[row + col] = True
+            visited_diag2[row - col + N] = True
+
+            cnt += recursive(row + 1, N, visited_col, visited_diag1, visited_diag2)
+
+            visited_col[col] = False
+            visited_diag1[row + col] = False
+            visited_diag2[row - col + N] = False
+
+    return cnt
         
 
 def solve():
     N = int(input())
-    location = set()
-    recursive(location, N, 0)
+
+    visited_col = [False] * N
+    visited_diag1 = [False] * (2 * N)
+    visited_diag2 = [False] * (2 * N)
+    
+    return recursive(0, N, visited_col, visited_diag1, visited_diag2)
 
 
 T = int(input())
 for test_case in range(1, T + 1):
-    ans = 0
-    solve()
-    print(f"#{test_case} {ans}")
+    print(f"#{test_case} {solve()}")

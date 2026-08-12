5cd278a3faa2855ff51ac05e1e32925b9bba8095
Seongyun <120894109+Timeisfast@users.noreply.github.com>
2026-08-12 14:07:38 +0900

    [D3] Title: [S/W 문제해결 기본] 4일차 - 거듭 제곱, Time: 59 ms, Memory: 52,480 KB -BaekjoonHub

--- seongyun/SWEA/D3/1217. ［S／W 문제해결 기본］ 4일차 － 거듭 제곱/［S／W 문제해결 기본］ 4일차 － 거듭 제곱.py
+++ seongyun/SWEA/D3/1217. ［S／W 문제해결 기본］ 4일차 － 거듭 제곱/［S／W 문제해결 기본］ 4일차 － 거듭 제곱.py
@@ -1,9 +1,12 @@
-def power(n, i):
+def power(n, i, N, M):
     if i == M:
         return n
-    return power(n * N, i + 1)
+    return power(n * N, i + 1, N, M)
+
+def solve():
+    N, M = map(int, input().split())
+    return power(1, 0, N, M)
 
 for test_case in range(1, 11):
     T = int(input())
-    N, M = map(int, input().split())
-    print(f"#{test_case} {power(N, 1)}")
+    print(f"#{test_case} {solve()}")

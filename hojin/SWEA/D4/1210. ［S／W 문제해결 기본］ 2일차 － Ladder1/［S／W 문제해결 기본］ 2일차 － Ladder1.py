ff43c58404c76c3ad29afc591e27bb61fe1dcb00
LeeHojin <141011143+LeeHojin165@users.noreply.github.com>
2026-08-13 14:32:05 +0900

    [D4] Title: [S/W 문제해결 기본] 2일차 - Ladder1, Time: 105 ms, Memory: 60,544 KB -BaekjoonHub

--- hojin/SWEA/D4/1210. ［S／W 문제해결 기본］ 2일차 － Ladder1/［S／W 문제해결 기본］ 2일차 － Ladder1.py
+++ hojin/SWEA/D4/1210. ［S／W 문제해결 기본］ 2일차 － Ladder1/［S／W 문제해결 기본］ 2일차 － Ladder1.py
@@ -17,9 +17,6 @@ for test_case in range(1,T+1):
             #방향이 1인상태로 온거랑
             if direction == 1:
                 direction = 0
-                curr_r += dr[direction]
-                curr_c += dc[direction]
-                continue
             #방량이 0인상태로 온거
             elif direction == 0:
                 #이때 오른 쪽을 검사
@@ -27,20 +24,13 @@ for test_case in range(1,T+1):
                 #1이면 방향을 튼다.
                 if ladder[curr_r][right] == 1:
                     direction = 2
-                    curr_r += dr[direction]
-                    curr_c += dc[direction]
-                    continue
                 else:
-                    curr_r += dr[direction]
-                    curr_c += dc[direction]
-                    continue
+                    pass
         elif curr_c == 99:
             # 방향이 2인상태로 온거랑
             if direction == 2:
                 direction = 0
-                curr_r += dr[direction]
-                curr_c += dc[direction]
-                continue
+
             # 방량이 0인상태로 온거
             elif direction == 0:
                 # 이때 왼 쪽을 검사
@@ -48,54 +38,35 @@ for test_case in range(1,T+1):
                 # 1이면 방향을 튼다.
                 if ladder[curr_r][left] == 1:
                     direction = 1
-                    curr_r += dr[direction]
-                    curr_c += dc[direction]
-                    continue
                 else:
-                    curr_r += dr[direction]
-                    curr_c += dc[direction]
-                    continue
+                    pass
         else:
             right = curr_c + 1
             left = curr_c - 1
             #둘이 동시에 1이면 가로로 진행중이라는 뜻이므로 그대로 pass
             if ladder[curr_r][left] == 1 and ladder[curr_r][right] == 1:
-                curr_r += dr[direction]
-                curr_c += dc[direction]
-                continue
+                pass
             #왼쪽이 1이고 오른쪽이 1이 아닐때는 오른쪽으로 진행해왔거나 위로 진행해왔을때
             elif ladder[curr_r][left] == 1 and ladder[curr_r][right] != 1:
                 #위로 진행해왔으면 왼쪽으로
                 if direction == 0:
                     direction = 1
-                    curr_r += dr[direction]
-                    curr_c += dc[direction]
-                    continue
                 #오른쪽이면 위로
                 elif direction == 2:
                     direction = 0
-                    curr_r += dr[direction]
-                    curr_c += dc[direction]
-                    continue
             #오른쪽만 1이면 왼쪽으로 진행해왔거나 위로 진행해왔을때
             elif ladder[curr_r][left] != 1 and ladder[curr_r][right] == 1:
                 # 위로 진행해왔으면 오른쪽으로
                 if direction == 0:
                     direction = 2
-                    curr_r += dr[direction]
-                    curr_c += dc[direction]
-                    continue
                 elif direction == 1:
                     direction = 0
-                    curr_r += dr[direction]
-                    curr_c += dc[direction]
-                    continue
             #다 아니면 둘다 0인거라 그냥 위로
             else:
                 direction = 0
-                curr_r += dr[direction]
-                curr_c += dc[direction]
-                continue
-                    
+
+
+        curr_r += dr[direction]
+        curr_c += dc[direction]
     result = curr_c
     print(f'#{test_case} {result}')

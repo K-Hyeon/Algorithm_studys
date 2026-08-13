f5e90322cd59a2ac3f4a91c50ea1a319ff2b9137
Seongyun <120894109+Timeisfast@users.noreply.github.com>
2026-08-13 17:27:47 +0900

    [Unrated] Title: [모의 SW 역량테스트] 홈 방범 서비스, Time: 668 ms, Memory: 70,784 KB -BaekjoonHub

--- seongyun/SWEA/Unrated/2117. ［모의 SW 역량테스트］ 홈 방범 서비스/［모의 SW 역량테스트］ 홈 방범 서비스.py
+++ seongyun/SWEA/Unrated/2117. ［모의 SW 역량테스트］ 홈 방범 서비스/［모의 SW 역량테스트］ 홈 방범 서비스.py
@@ -1,46 +1,37 @@
 from collections import deque
-import pprint
 
 def solve():
     N, M = map(int, input().split())
     town = [list(map(int, input().split())) for _ in range(N)]
 
+    dx = (1, -1, 0, 0)
+    dy = (0, 0, 1, -1)
     max_home = 0
     for r in range(N):
         for c in range(N):
-            cnt = 0
-            if town[r][c] == 1:
-                cnt += 1
-
             visited = [[False for _ in range(N)] for _ in range(N)]
-            dx = (1, -1, 0, 0)
-            dy = (0, 0, 1, -1)
-
-            q = deque()
-            q.append((r, c))
+            q = deque([(r, c)])
             visited[r][c] = True
-            k = 1
 
-            cost = cnt*M - (k*k + (k-1)*(k-1))
-            if cost >= 0:
-                max_home = max(max_home, cnt)
+            k = 0
+            cnt = 0
+
             while q:
                 length = len(q)
                 k += 1
                 for _ in range(length):
                     cur_x, cur_y = q.popleft()
-                    for i in range(4):
-                        nx = cur_x + dx[i]
-                        ny = cur_y + dy[i]
+                    if town[cur_x][cur_y] == 1:
+                        cnt += 1
 
+                    for i in range(4):
+                        nx, ny = cur_x + dx[i], cur_y + dy[i]
                         if 0 <= nx < N and 0 <= ny < N and not visited[nx][ny]:
                             visited[nx][ny] = True
-                            if town[nx][ny] == 1:
-                                cnt += 1
                             q.append((nx, ny))
 
-                cost = cnt*M - (k*k + (k-1)*(k-1))
-                if cost >= 0:
+                cost = k * k + (k - 1) * (k - 1)
+                if cnt * M >= cost:
                     max_home = max(max_home, cnt)
 
     return max_home

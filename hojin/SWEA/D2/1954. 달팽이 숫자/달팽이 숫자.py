677cf5d07fe187a3af1972e7894a4c3221a37891
LeeHojin <141011143+LeeHojin165@users.noreply.github.com>
2026-08-31 16:02:36 +0900

    [D2] Title: 달팽이 숫자, Time: 81 ms, Memory: 52,352 KB -BaekjoonHub

--- hojin/SWEA/D2/1954. 달팽이 숫자/달팽이 숫자.py
+++ hojin/SWEA/D2/1954. 달팽이 숫자/달팽이 숫자.py
@@ -1,22 +1,35 @@
-T = int(input())
-for test_case in range(1, T+1):
-    #우하좌상
-    n = int(input())
-    arr = [[0]*n for _ in range(n)]
-    dr = [0,1,0,-1]
-    dc = [1,0,-1,0]
-    dir = 0
-    r,c =0,0
-    print(f"#{test_case}")
-    for num in range(1,n*n+1):
-        arr[r][c] = num
-        nr, nc = r + dr[dir], c + dc[dir]
-        if 0<=nr<n and 0<=nc<n and arr[nr][nc] == 0:
-            r,c = nr,nc
-        else:
-            dir = (dir+1) % 4
-            r,c = r + dr[dir], c + dc[dir]
+from collections import deque
 
-    for row in arr:
-        [print(item, end=" ") for item in row]
-        print()
+T = int(input())
+# 여러개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
+for test_case in range(1, T + 1):
+    N = int(input())
+
+    dx = [0, 1, 0, -1] # 우(0), 하(1), 좌(2), 상(3)
+    dy = [1, 0, -1, 0]
+
+    rotate_map = [[0] * N for _ in range(N)]
+    search_queue = deque()
+
+    search_queue.append((0, 0, 0, 1)) # (x, y, 현재 direction, 현재 번호)
+    while search_queue:
+        cur_x, cur_y, cur_dirrection, cur_num = search_queue.popleft()
+        rotate_map[cur_x][cur_y] = cur_num
+
+        new_x = cur_x + dx[cur_dirrection]
+        new_y = cur_y + dy[cur_dirrection]
+
+        if 0 <= new_x < N and 0 <= new_y < N and rotate_map[new_x][new_y] == 0:
+            search_queue.append((new_x, new_y, cur_dirrection, cur_num + 1))
+
+        # new 방향이 map범위를 넘어가거나, 이미 값이 채워진 상태일 때, 
+        elif cur_num != N*N: # 현재 숫자가 N*N과 같다면 map이 모두 채워진 상태이므로 continue 
+            next_dirrection = (cur_dirrection + 1) % 4
+            new_x = cur_x + dx[next_dirrection]
+            new_y = cur_y + dy[next_dirrection]
+            search_queue.append((new_x, new_y, next_dirrection, cur_num + 1))
+
+
+    print(f"#{test_case}")
+    for i in range(N):
+        print(*rotate_map[i])
